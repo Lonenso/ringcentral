73d7501f3539876c05cda53c4dddbcbff67b3d9c
Lonenso <xyx19961107@qq.com>
2020-07-14 23:06:35 +0800

    implement locking

--- main.go
+++ main.go
@@ -7,21 +7,46 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"strings"
+	"sync"
+	"time"
 
 	"github.com/gin-contrib/multitemplate"
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"github.com/satori/go.uuid"
 )
 
 var db *gorm.DB
 var dataPath string
+var kv sync.Map
+var sb strings.Builder
+var interval = time.Second * 5
+var lease = time.Minute
 
 type File struct {
 	ID       uint `gorm:"primary_key"`
 	Filename string
 	Path     string `json:"-"`
 }
+
+type Lock struct {
+	SessionID string
+	CreatedAt time.Time
+}
+
+
+func (l *Lock) String() string {
+	return fmt.Sprintf("object locked by %v since %v", l.SessionID, l.CreatedAt)
+}
+
+func (l *Lock) Expired() bool {
+	return time.Now().Sub(l.CreatedAt) > lease
+}
+
 
 // newFile
 func newFile(c *gin.Context) {
@@ -50,9 +75,20 @@ func newFile(c *gin.Context) {
 
 // editFile
 func editFile(c *gin.Context) {
+	var sessionID string
+	session := sessions.Default(c)
+	si := session.Get("sessionID")
+	if si == nil {
+		sessionID = uuid.NewV4().String()
+	} else {
+		sessionID = si.(string)
+	}
+	session.Set("sessionID", sessionID)
+	_ = session.Save()
+
 	id := c.Param("id")
 	var file File
-	err := db.First(&file, id).Error
+	err := db.Debug().First(&file, id).Error
 	if err != nil {
 		c.String(http.StatusNotFound, fmt.Sprintf("Sorry, File id: %v not found", id))
 		return
@@ -62,8 +98,23 @@ func editFile(c *gin.Context) {
 		c.String(http.StatusNotFound, fmt.Sprintf("Sorry, File: %v not found", file.Filename))
 		return
 	}
+
+	// trylock
+	if value, ok := kv.Load(id); ok {
+		l := value.(*Lock)
+		if l.SessionID != sessionID {
+			c.String(http.StatusLocked, fmt.Sprintf("Sorry, File: %v is edited by another user", id))
+			return
+		}
+	} else {
+		kv.Store(id, &Lock{
+			SessionID: sessionID,
+			CreatedAt: time.Now(),
+		})
+	}
+
 	if c.Request.Method == "GET" {
-		c.HTML(http.StatusOK, "draft", gin.H{"filename": file.Filename, "content": string(b)})
+		c.HTML(http.StatusOK, "draft", gin.H{"filename": file.Filename, "content": string(b), "id": id})
 		return
 	}
 
@@ -76,6 +127,7 @@ func editFile(c *gin.Context) {
 	}
 
 	c.String(http.StatusOK, fmt.Sprintf("update file: %v successfully", file.Filename))
+	kv.Delete(id)
 }
 
 // viewFileList
@@ -102,6 +154,19 @@ func downloadFile(c *gin.Context) {
 	_, _ = c.Writer.Write(b)
 }
 
+func showKV(c *gin.Context) {
+	sb.Reset()
+	sb.WriteString("Here shows locked files\n")
+	kv.Range(func(key, value interface{}) bool {
+		l := value.(*Lock)
+		sb.WriteString(fmt.Sprintf("file: %v, %v\n",key, l))
+		return true
+	})
+	c.String(http.StatusOK, sb.String())
+	return
+}
+
+
 func setupDB(dbPath string) {
 	var err error
 	db, err = gorm.Open("sqlite3", dbPath)
@@ -110,6 +175,7 @@ func setupDB(dbPath string) {
 	}
 	db.AutoMigrate(File{})
 }
+
 
 func setupText(dataPath string) {
 	err := os.Mkdir(dataPath, os.ModePerm)
@@ -152,7 +218,7 @@ func setupRender() multitemplate.Renderer {
 </head>
 <body>
 <div>
-   <form id="myForm" action="/" method="post">
+   <form id="myForm" action="/draft/{{.id}}" method="post">
        Name:    <input name="name" type="text" readonly value="{{.filename}}"><br>
        Content: <textarea cols="33" rows="5" name="content">{{.content}}</textarea><br>
        <input type="submit" value="Update">
@@ -184,6 +250,9 @@ func setupRender() multitemplate.Renderer {
 
 func setupRouter() *gin.Engine {
 	r := gin.Default()
+	store := cookie.NewStore([]byte("ringcentral"))
+	r.Use(sessions.Sessions("mysession", store))
+
 	r.HTMLRender = setupRender()
 	r.GET("/", newFile)
 	r.POST("/", newFile)
@@ -191,7 +260,23 @@ func setupRouter() *gin.Engine {
 	r.GET("/text", viewFile)
 	r.GET("/draft/:id", editFile)
 	r.POST("/draft/:id", editFile)
+	r.GET("/debug", showKV)
 	return r
+}
+
+func setupKV() {
+	t := time.NewTicker(interval)
+	go func() {
+		for range t.C {
+			kv.Range(func(key, value interface{}) bool {
+				l := value.(*Lock)
+				if l.Expired() {
+					kv.Delete(key)
+				}
+				return true
+			})
+		}
+	}()
 }
 
 func main() {
@@ -221,6 +306,7 @@ func main() {
 
 	dataPath = filepath.Join(cwd, "data")
 	setupText(dataPath)
+	setupKV()
 
 	r := setupRouter()
 	log.Fatalf("serve failed, %v", r.Run(":8080"))
